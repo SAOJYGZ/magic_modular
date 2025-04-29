dca5a704b0c3db6ffcf475a9149149097a5c5970
SAOJYGZ <89456542+SAOJYGZ@users.noreply.github.com>
2025-04-29 13:55:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app_pages.hedge_params import render as render_hedge_params
 from app_pages.product_trend import render as render_product_trend
 from app_pages.situational_ana import render as render_situational_ana
 from app_pages.acknowledgements import render as render_acknowledgements
-from app_pages.test import render as render_test
+# from app_pages.test import render as render_test
 
 st.set_page_config(page_title="交易分析仪表板",layout="wide")
 st.sidebar.title("功能导航")
@@ -18,7 +18,7 @@ elif page=="客户产品趋势分析":
     render_product_trend()
 elif page == "情景分析":
     render_situational_ana()
-elif page == '测试专用':
-    render_test()
+# elif page == '测试专用':
+#     render_test()
 else:
     render_acknowledgements()
